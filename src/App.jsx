0a17cb84c9f4cee7ee51288c21d1f50cec9d798b
DQCoding1 <diegoabquintero@gmail.com>
2023-08-18 00:19:13 -0500

    example 2 when react renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    setTimeout(() => {
-      console.log(3);
-    },0)
+    useEffect(() => {
+        // EXAMPLE 1
+        // setTimeout(() => {
+        //   console.log(3);
+        // },0)
 
-    setTimeout(() => {
-      console.log(2);
-    },0)
+        // setTimeout(() => {
+        //   console.log(2);
+        // },0)
 
-    const p = new Promise((res) => {
-      res("p")
-    })
-    p.then((res) => {
-      console.log(res)
-    })
+        // const p = new Promise((res) => {
+        //   res("p")
+        // })
+        // p.then((res) => {
+        //   console.log(res)
+        // })
+        
+        // console.log(1);
+    }, []);
 
-    console.log(1);
-  }, [])
+    useEffect(() => {
+        // EXAMPLE 2 When React Renders
+        console.log(count);
+        setCount(9);
+        setCount(8);
+        setCount(7);
+        setCount(6);
+    });
 
-  return (
-    <div>
-
-    </div>
-  )
+    return <div>{count}</div>;
 }
 
-export default App
+export default App;
